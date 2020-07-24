77995bd27ff14871efd141944ab83fbb30223f3b
Amirali Esfandiari <amiralinull@gmail.com>
2020-07-24 22:53:34 +0430

    add initial setup action instead of manual setup

--- readme.md
+++ readme.md
@@ -5,6 +5,5 @@ a server thing to send your data to your telegram account using a bot
 - install dependencies: `flask`, `requests`
 - get a telegram bot token from `@BotFather`
 - get your user id from `@JsonDumpBot`
-- Edit `config.py` file
 - just run it and available on `127.0.0.1:5000`
 - Or use docker to build an image and run it
--- telegramify/main.py
+++ telegramify/main.py
@@ -1,43 +1,70 @@
-import os, sys
+import sys
 from urllib.parse import quote
 import hashlib
 import logging
+import uuid
 
 import requests
 
-from flask import Flask, request, render_template
-
-import config
+import toml
+from flask import Flask, request, render_template, flash, redirect, url_for
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = config.secret_key
+try:
+    config = toml.loads(open('config.toml', 'r').read())
+    app.config['tgBotToken'] = config['tgBotToken']
+    app.config['tgUserID'] = config['tgUserID']
+    app.config['hashedPassword'] = config['hashedPassword']
+    is_init = False
+except:
+    is_init = True
 
-
-tgBotToken = config.TG_BOT_TOKEN
-tgUserID = config.TG_USER_ID
-hashedPassword = config.HASHED_PASSWORD
-
-if not (tgBotToken or tgUserID or hashedPassword):
-    logging.info("Please add `TG_BOT_TOKEN`, `TG_USER_ID`, `HASHED_PASSWORD` to your config.py and try again")
-    sys.exit(1)
-
-baseUrl = "https://api.telegram.org/bot" + tgBotToken + "/sendMessage?chat_id=" + tgUserID + "&text="
-
+app.config['SECRET_KEY'] = uuid.uuid4().bytes
 
 @app.route('/')
 def index():
+    if is_init == True:
+        return redirect(url_for('init'))
+
     return render_template("index.html")
+
+@app.route('/init', methods=['GET', 'POST'])
+def init():
+    if request.method == 'GET':
+        return render_template("init.html")
+    else:
+        tgBotToken = request.form.get('BotToken')
+        tgUserID = request.form.get('UserID')
+        password = request.form.get('Password')
+
+        if not (tgBotToken and tgUserID and password):
+            flash('Please fill every field')
+            return render_template("init.html", color='red')
+
+        hashedPassword = hashlib.sha256(password.encode()).hexdigest()
+        config = toml.dumps({'tgBotToken': tgBotToken, 'tgUserID': tgUserID, 'hashedPassword': hashedPassword})
+        with open('config.toml', 'w') as f:
+            f.write(config)
+
+        app.config['tgBotToken'] = tgBotToken
+        app.config['tgUserID'] = tgUserID
+        app.config['hashedPassword'] = hashedPassword
+
+        flash("Config file successfully changed")
+        return render_template('init.html', color='green')
 
 @app.route('/send', methods=['POST'])
 def send():
+    baseUrl = "https://api.telegram.org/bot" + app.config['tgBotToken'] + "/sendMessage?chat_id=" + app.config['tgUserID'] + "&text="
+
     sendedHashedPassword = hashlib.sha256(request.form.get('password').encode()).hexdigest()
     content = request.form.get('content')
     if not content:
         return render_template("response.html", code=204, legend="No Content"), 204
-    if not (sendedHashedPassword == hashedPassword):
+    if not (sendedHashedPassword == app.config['hashedPassword']):
         return render_template("response.html", code=401, legend="Unauthorized"), 401
-    
+
     url = baseUrl + str(quote(content))
     r = requests.get(url)
     if r.status_code == 200:
